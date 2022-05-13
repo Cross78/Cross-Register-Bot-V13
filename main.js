e2f5848b626cc0e47e9aacf85a3f79f346fc5509
CrossC5 <88112517+CrossC5@users.noreply.github.com>
2022-05-14 00:33:03 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js")
-const client = new Discord.Client({ 'intents': [32767, "GUILD_INVITES"]});
+const client = new Discord.Client({ 'intents': [32767]});
 const ayar = require("./settings.js")
 const fs = require("fs");
 require('./util/Loader.js')(client);
